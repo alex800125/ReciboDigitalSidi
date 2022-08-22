0f535e5727630617d121e508f71f7cca21a0c72b
alex800125 <alex800125@hotmail.com>
2022-08-22 00:02:07 -0300

    [US_7] Login Screen
    Campos de 'login' e 'senha' criados + ações dos botões criadas, porém apenas um toast é exibido.

--- app/src/main/java/com/example/recibodigitalalexventurini/MainActivity.kt
+++ app/src/main/java/com/example/recibodigitalalexventurini/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.recibodigitalalexventurini
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+
+        var userEmailOrPhoneEditText = findViewById<EditText>(R.id.user_email)
+
+        findViewById<Button>(R.id.login)
+            .setOnClickListener {
+                executeLogin()
+            }
+        findViewById<Button>(R.id.new_account)
+            .setOnClickListener {
+                executeNewAccount()
+            }
+        findViewById<TextView>(R.id.forgot_password)
+            .setOnClickListener {
+                executeForgotPassword()
+            }
+    }
+
+    private fun executeLogin() {
+        // TODO implement
+        Toast.makeText(this, "Login executado", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun executeNewAccount() {
+        // TODO implement
+        Toast.makeText(this, "New Account executado", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun executeForgotPassword() {
+        // TODO implement
+        Toast.makeText(this, "Forgot Password executado", Toast.LENGTH_SHORT).show()
     }
 }
